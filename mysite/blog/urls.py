e34782a7003d21aa72d0a6e32dfb89724d70660c
Kheireddine Sadok <kheireddine.sadok@newaccess.ch>
2019-08-15 15:16:47 +0200

    finished chapitre 1

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'blog'
 
 urlpatterns = [
-    path('',views.post_list,name='post_list'),
+    
+    #path('',views.post_list,name='post_list'), # function based view
+    path('', views.PostListView.as_view(), name='post_list'), #class-based view
     path('<int:year>/<int:month>/<int:day>/<slug:post>',views.post_detail,name='post_detail'),
+    
 ]
